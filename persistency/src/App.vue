3a88cdcebc106c4241e89a742d8de58e74b06137
JoseRandoDEV <joseandresrando@gmail.com>
2025-04-19 11:56:08 -0300

    Terminado Capitulo watch

---
+++ persistency/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Notas</h1>
+  <input type="text" v-model="note"/>
+</template>
+
+<script lang="ts" setup>
+import { watch, ref} from 'vue';
+    const note = ref('')
+    watch(note, () => alert("Valor Modificado: " + note.value)
+        )
+</script>
+
+<style>
+
+</style>
