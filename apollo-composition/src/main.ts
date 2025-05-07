ed61465ffe92ec21fd68a0085997591f53aa70ac
JOSE RANDO <JOSEANDRESRANDO@GMAIL.COM>
2025-05-07 10:22:04 -0300

    Se actualizo 7/5

---
+++ apollo-composition/src/main.ts
@@ -0,0 +1,48 @@
+import { createApp, h } from 'vue'
+import App from './App.vue'
+import { ApolloClient, createHttpLink, InMemoryCache, split } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql'
+})
+
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:4000/graphql',
+    options: {
+        reconnect: true
+    }
+})
+
+const cache = new InMemoryCache()
+
+const link = split(
+    ({query})=> {
+        const definition = getMainDefinition(query)
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        )
+    },
+    wsLink,
+    httpLink
+)
+
+const apolloClient = new ApolloClient({
+    link: link,
+    cache
+})
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+})
+
+
+createApp({
+    
+    render: () => h(App)
+
+}).use(apolloProvider).mount('#app')
---
+++ control/src/components/childComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Porcentaje Completado: {{ percentage }}%</h1>
+</template>
+
+<script lang="ts" setup>
+import { onMounted,ref,defineExpose} from 'vue';
+const percentage = ref(0);
+
+onMounted(() => {
+    //trae el valor predeterminado al servicio
+    percentage.value = 24;
+});
+const recalculate = () => {
+    percentage.value = 42;
+};
+
+defineExpose({recalculate});
+
+</script>
