5eae7e8578115c725643262fdb0a412b008691d5
JoseRandoDEV <joseandresrando@gmail.com>
2025-05-09 09:44:07 -0300

    Fin Clase 33

---
+++ locatecontent/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <select v-model="$i18n.locale">
+      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale"> {{ locale }}</option>
+    </select>
+  </div>
+  <h1>{{ $t("message.hello",{name: username, surname: surname}) }}</h1>
+  <p>{{ $t("message.content") }}</p>
+  <p>{{ $t("message.products", {count: 0}) }}</p>
+  <p>{{ $t("message.comments", 5) }}</p>
+
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue'
+
+const username = ref('Jose')
+const surname = ref('Rando')
+
+</script>
+
+<style>
+
+</style>
+
---
+++ locatecontent/src/lang/messages.ts
@@ -0,0 +1,18 @@
+export const messages = {
+    en: {
+        message: {
+            hello: 'Hello {name} {surname}!',
+            content: 'We are using i18n in our app',
+            products: 'no products | one product | {count} products',
+            comments: 'comment | comments',
+        }
+    },
+    es: {
+        message: {
+            hello: 'Hola {name} {surname}!',
+            content: 'Estamos usando i18n en nuestra aplicación',
+            products: 'no hay productos | un producto | {count} productos',
+            comments: 'comentario | comentarios',
+        }
+    }
+}
