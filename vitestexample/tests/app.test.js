2f593719b301fae2cea40437e7b469c679df8dfc
JoseRandoDEV <joseandresrando@gmail.com>
2025-05-22 10:02:51 -0300

    Actualizado al 22-5-25

---
+++ vitestexample/tests/app.test.js
@@ -0,0 +1,20 @@
+import App from '../src/App.vue';
+import { mount } from '@vue/test-utils';
+
+describe('App.vue', () => {
+    it('the component render the initial state', () => {
+        const wrapper = mount(App);
+        if(expect(wrapper.find('h1').exists())) {
+            expect(wrapper.text()).toBe('0')
+        }
+        if(expect(wrapper.find('button').exists())) {
+            expect(wrapper.text()).toBe('Increment')
+        }
+    })
+    it('the increments button and ones to counter', async () => {
+        const wrapper = mount(App);
+        await wrapper.find('button').trigger('click');
+        expect(wrapper.find('h1').text()).toBe('1');
+    })
+})   
+    
