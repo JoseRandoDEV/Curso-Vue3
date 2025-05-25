25d656183e8886493663549342b75f36e126b71e
JoseRandoDEV <joseandresrando@gmail.com>
2025-05-24 21:46:40 -0300

    Nuxt Clase 1

---
+++ nuxt/routingexample/layouts/projects.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Projects Pages</h1>
+    <hr />
+    <slot />
+  </div>
+</template>
---
+++ nuxt/routingexample/pages/projects/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout name="projects">
+    <h1>Project Number {{ projectId }}</h1>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const projectId = ref();
+projectId.value = route.params.id;
+</script>
