39a6a0d0ed1b75d95191da23f732158070412b78
JoseRandoDEV <joseandresrando@gmail.com>
2025-05-29 12:29:45 -0300

    Quasar 1 Pasos

---
+++ Ejemplo/src/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row justify="start" align="end">
+      <v-col class="red">Celda 1 - 1</v-col>
+      <v-col class="red">Celda 1 - 2</v-col>
+      <v-col class="red">Celda 1 - 3</v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col class="blue">Celda 2 - 1</v-col>
+      <v-col class="blue">Celda 2 - 2</v-col>
+    </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col class="brown">Celda 3 - 1</v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.red {
+  background-color: red;
+}
+.blue {
+  background-color: blue;
+}
+.brown {
+  background-color: brown;
+}
+</style>
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message name="Yo" :text="['Welcome to quasar!!']" sent />
+      <q-chat-message name="El" :text="['Thanks..']" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
